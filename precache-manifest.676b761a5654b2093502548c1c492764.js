1086327061247837344b75f98da7dd37140db174
Gabor Cserto <csertog@gmail.com>
2020-08-07 14:07:15 +0200

    Updates

---
+++ precache-manifest.676b761a5654b2093502548c1c492764.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a37b66dfffcdfc6202cf8a4cf63033a8",
+    "url": "/tv-guide/index.html"
+  },
+  {
+    "revision": "c8ea125f800b32aad584",
+    "url": "/tv-guide/static/css/main.17dcd975.chunk.css"
+  },
+  {
+    "revision": "7bda5950bdfbeed791d6",
+    "url": "/tv-guide/static/js/2.1ac9af78.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/tv-guide/static/js/2.1ac9af78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8ea125f800b32aad584",
+    "url": "/tv-guide/static/js/main.921cafad.chunk.js"
+  },
+  {
+    "revision": "991317da9d517f3ef82f",
+    "url": "/tv-guide/static/js/runtime-main.710eb3d5.js"
+  }
+]);
